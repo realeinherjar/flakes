7b0d9757b4bc108b6eefa680c4066269fb901ade
Einherjar <realeinherjar@proton.me>
2023-06-18 11:44:41 +0000

    Bat btop cmus confs (#6)
    * fix: btop conf and catppuccin theme
    
    * fix: bat config and catppuccin theme
    
    * fix: cmus config
    
    * fix: flake update
    
    * fix: hash256 catppuccin bat theme

--- modules/programs/common/search/default.nix
+++ modules/programs/common/search/default.nix
@@ -6,7 +6,6 @@
       mlocate
       tree
       fd
-      bat
       ripgrep
       exa
       mediainfo
@@ -18,6 +17,19 @@
     fzf = {
       enable = true;
       enableFishIntegration = true;
+    };
+    bat = {
+      enable = true;
+      config = {
+        italic-text = "always";
+        theme = "catppuccin";
+      };
+      themes = {
+        catppuccin = builtins.readFile (pkgs.fetchurl {
+          url = "https://github.com/catppuccin/bat/raw/main/Catppuccin-mocha.tmTheme";
+          sha256 = "a8c40d2466489a68ebab3cbb222124639221bcf00c669ab45bab242cbb2783fc";
+        });
+      };
     };
     zoxide = {
       enable = true;
