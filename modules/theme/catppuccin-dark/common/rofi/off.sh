93bb255d6013423b32257d8bf9bb39fabe9ca6a0
Einherjar <realeinherjar@proton.me>
2023-06-18 10:28:27 +0000

    fix: powericons unicode symbols (#4)

--- modules/theme/catppuccin-dark/common/rofi/off.sh
+++ modules/theme/catppuccin-dark/common/rofi/off.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 # All supported choices
-all=(shutdown reboot suspend hibernate  logout)
+all=(shutdown reboot suspend hibernate logout)
 
 # By default, show all (i.e., just copy the array)
 show=("${all[@]}")
@@ -17,9 +17,9 @@ texts[shutdown]="shutdown"
 
 declare -A icons
 icons[suspend]="\u23fe"
-icons[logout]="\uf842"
-icons[hibernate]="\uf7c9"
-icons[reboot]="\ufc07"
+icons[logout]="\u2196"
+icons[hibernate]="\u2638"
+icons[reboot]="\u2678"
 icons[shutdown]="\uf011"
 icons[cancel]="\u00d7"
 
@@ -38,59 +38,54 @@ dryrun=false
 showsymbols=true
 
 function check_valid {
-    option="$1"
-    shift 1
-    for entry in "${@}"
-    do
-        if [ -z "${actions[$entry]+x}" ]
-        then
-            echo "Invalid choice in $1: $entry" >&2
-            exit 1
-        fi
-    done
+	option="$1"
+	shift 1
+	for entry in "${@}"; do
+		if [ "${actions[$entry]+x}" = "" ]; then
+			echo "Invalid choice in $1: $entry" >&2
+			exit 1
+		fi
+	done
 }
-
 
 # Define the messages after parsing the CLI options so that it is possible to
 # configure them in the future.
 
 function write_message {
-    icon="<span font_size=\"medium\">$1</span>"
-    text="<span font_size=\"medium\">$2</span>"
-    if [ "$showsymbols" = "true" ]
-    then
-        echo -n "\u200e$icon \u2068$text\u2069"
-    else
-        echo -n "$text"
-    fi
+	icon="<span font_size=\"medium\">$1</span>"
+	text="<span font_size=\"medium\">$2</span>"
+	if [ "$showsymbols" = "true" ]; then
+		echo -n "\u200e$icon \u2068$text\u2069"
+	else
+		echo -n "$text"
+	fi
 }
 
 function print_selection {
-    echo -e "$1" | $(read -r -d '' entry; echo "echo $entry")
+	echo -e "$1" | "$(
+		read -r -d '' entry
+		echo "echo $entry"
+	)"
 }
 
 declare -A messages
 declare -A confirmationMessages
-for entry in "${all[@]}"
-do
-    messages[$entry]=$(write_message "${icons[$entry]}" "${texts[$entry]^}")
+for entry in "${all[@]}"; do
+	messages[$entry]=$(write_message "${icons[$entry]}" "${texts[$entry]^}")
 done
-for entry in "${all[@]}"
-do
-    confirmationMessages[$entry]=$(write_message "${icons[$entry]}" "Yes, ${texts[$entry]}")
+for entry in "${all[@]}"; do
+	confirmationMessages[$entry]=$(write_message "${icons[$entry]}" "Yes, ${texts[$entry]}")
 done
 confirmationMessages[cancel]=$(write_message "${icons[cancel]}" "No, cancel")
 
-if [ $# -gt 0 ]
-then
-    # If arguments given, use those as the selection
-    selection="${@}"
+if [ $# -gt 0 ]; then
+	# If arguments given, use those as the selection
+	selection="${@}"
 else
-    # Otherwise, use the CLI passed choice if given
-    if [ -n "${selectionID+x}" ]
-    then
-        selection="${messages[$selectionID]}"
-    fi
+	# Otherwise, use the CLI passed choice if given
+	if [ "${selectionID+x}" != "" ]; then
+		selection="${messages[$selectionID]}"
+	fi
 fi
 
 # Don't allow custom entries
@@ -98,52 +93,43 @@ echo -e "\0no-custom\x1ftrue"
 # Use markup
 echo -e "\0markup-rows\x1ftrue"
 
-if [ -z "${selection+x}" ]
-then
-    echo -e "\0prompt\x1fPower menu"
-    for entry in "${show[@]}"
-    do
-        echo -e "${messages[$entry]}\0icon\x1f${icons[$entry]}"
-    done
+if [ "${selection+x}" = "" ]; then
+	echo -e "\0prompt\x1fPower menu"
+	for entry in "${show[@]}"; do
+		echo -e "${messages[$entry]}\0icon\x1f${icons[$entry]}"
+	done
 else
-    for entry in "${show[@]}"
-    do
-        if [ "$selection" = "$(print_selection "${messages[$entry]}")" ]
-        then
-            # Check if the selected entry is listed in confirmation requirements
-            for confirmation in "${confirmations[@]}"
-            do
-                if [ "$entry" = "$confirmation" ]
-                then
-                    # Ask for confirmation
-                    echo -e "\0prompt\x1fAre you sure"
-                    echo -e "${confirmationMessages[$entry]}\0icon\x1f${icons[$entry]}"
-                    echo -e "${confirmationMessages[cancel]}\0icon\x1f${icons[cancel]}"
-                    exit 0
-                fi
-            done
-            # If not, then no confirmation is required, so mark confirmed
-            selection=$(print_selection "${confirmationMessages[$entry]}")
-        fi
-        if [ "$selection" = "$(print_selection "${confirmationMessages[$entry]}")" ]
-        then
-            if [ $dryrun = true ]
-            then
-                # Tell what would have been done
-                echo "Selected: $entry" >&2
-            else
-                # Perform the action
-                ${actions[$entry]}
-            fi
-            exit 0
-        fi
-        if [ "$selection" = "$(print_selection "${confirmationMessages[cancel]}")" ]
-        then
-            # Do nothing
-            exit 0
-        fi
-    done
-    # The selection didn't match anything, so raise an error
-    echo "Invalid selection: $selection" >&2
-    exit 1
+	for entry in "${show[@]}"; do
+		if [ "$selection" = "$(print_selection "${messages[$entry]}")" ]; then
+			# Check if the selected entry is listed in confirmation requirements
+			for confirmation in "${confirmations[@]}"; do
+				if [ "$entry" = "$confirmation" ]; then
+					# Ask for confirmation
+					echo -e "\0prompt\x1fAre you sure"
+					echo -e "${confirmationMessages[$entry]}\0icon\x1f${icons[$entry]}"
+					echo -e "${confirmationMessages[cancel]}\0icon\x1f${icons[cancel]}"
+					exit 0
+				fi
+			done
+			# If not, then no confirmation is required, so mark confirmed
+			selection=$(print_selection "${confirmationMessages[$entry]}")
+		fi
+		if [ "$selection" = "$(print_selection "${confirmationMessages[$entry]}")" ]; then
+			if [ "$dryrun" = true ]; then
+				# Tell what would have been done
+				echo "Selected: $entry" >&2
+			else
+				# Perform the action
+				"${actions[$entry]}"
+			fi
+			exit 0
+		fi
+		if [ "$selection" = "$(print_selection "${confirmationMessages[cancel]}")" ]; then
+			# Do nothing
+			exit 0
+		fi
+	done
+	# The selection didn't match anything, so raise an error
+	echo "Invalid selection: $selection" >&2
+	exit 1
 fi
