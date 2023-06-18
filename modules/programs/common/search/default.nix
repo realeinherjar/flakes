b81fc52d9df151b82dbc4384b1f7a2d3d0008fab
Einherjar <realeinherjar@proton.me>
2023-06-18 07:50:06 -0300

    fix: bat config and catppuccin theme

--- modules/programs/common/search/default.nix
+++ modules/programs/common/search/default.nix
@@ -6,7 +6,6 @@
       mlocate
       tree
       fd
-      bat
       ripgrep
       exa
       mediainfo
@@ -18,6 +17,22 @@
     fzf = {
       enable = true;
       enableFishIntegration = true;
+    };
+    bat = {
+      enable = true;
+      config = {
+        plain = true;
+        italic_text = true;
+        theme = "catppuccin";
+      };
+      themes = {
+        catppuccin = builtins.readFile (pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "477622171ec0529505b0ca3cada68fc9433648c6";
+          sha256 = "a8c40d2466489a68ebab3cbb222124639221bcf00c669ab45bab242cbb2783fc";
+        } + "/Catppuccin-mocha.tmTheme");
+      };
     };
     zoxide = {
       enable = true;
