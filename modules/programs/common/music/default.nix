ef81eb0299644f4273f3be931cb52ba6d54685c0
Einherjar <realeinherjar@proton.me>
2023-06-18 07:50:22 -0300

    fix: cmus config

--- modules/programs/common/music/default.nix
+++ modules/programs/common/music/default.nix
@@ -5,6 +5,10 @@
     packages = with pkgs; [
       cava
       cmus
+      opustags
     ];
   };
+  home.file.".config/cmus/rc".text = ''
+    bind -f common ^T push run opustags -s FIELD=VALUE -i {}
+  '';
 }
