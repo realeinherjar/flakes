a077ec553a2445d91b8464767d705502dd257330
Einherjar <realeinherjar@proton.me>
2023-06-25 04:27:30 +0200

    Create ssh-agent.nix

---
+++ modules/shell/fish/functions/ssh-agent.nix
@@ -0,0 +1,22 @@
+''
+function __ssh_agent_start -d "start a new ssh agent"
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  chmod 600 $SSH_ENV
+  source $SSH_ENV > /dev/null
+end
+
+function __ssh_agent_is_started -d "check if ssh agent is already started"
+	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+		source $SSH_ENV > /dev/null
+	end
+
+	if begin; test -z "$SSH_AGENT_PID"; and test -z "$SSH_CONNECTION"; end
+		return 1
+	end
+
+	ssh-add -l > /dev/null 2>&1
+	if test $status -eq 2
+		return 1
+	end
+end
+'';
