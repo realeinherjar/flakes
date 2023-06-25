b305630c1a46030bac4db19e947a6b1afbb5379a
Einherjar <realeinherjar@proton.me>
2023-06-25 06:03:28 +0200

    fix: filepaths

--- modules/programs/wayland/mpv/default.nix
+++ modules/programs/wayland/mpv/default.nix
@@ -7,7 +7,7 @@
     };
   };
   home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
-  home.file.".config/mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
+  home.file.".config/mpv/scripts/file_browser.lua".source = ./scripts/file_browser.lua;
   home.file.".config/mpv/scripts-opts/file_browser.conf".source = ./script-opts/file_browser.conf;
   home.file.".config/mpv/scripts/mpv_thumbnail_script_client_osc.lua".source = ./scripts/mpv_thumbnail_script_client_osc.lua;
   home.file.".config/mpv/scripts/mpv_thumbnail_script_server-1.lua".source = ./scripts/mpv_thumbnail_script_server-1.lua;
