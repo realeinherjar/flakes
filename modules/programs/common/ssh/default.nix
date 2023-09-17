b3022e5e66e23dc414c4c04c092db88ed6a264cc
Einherjar <realeinherjar@proton.me>
2023-09-17 10:41:42 -0300

    feat(ssh): onion and all hosts

--- modules/programs/common/ssh/default.nix
+++ modules/programs/common/ssh/default.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   programs.ssh = {
     enable = true;
     matchBlocks = {
@@ -12,6 +11,16 @@
         hostname = "gitlab.com";
         forwardX11 = false;
         forwardX11Trusted = false;
+      };
+      "onion" = {
+        hostname = "*.onion";
+        proxyCommand = "nc -xlocalhost:9050 %h %p";
+      };
+      "all" = {
+        hostname = "*";
+        forwardAgent = true;
+        forwardX11 = true;
+        IdentityFile = [ "~/.ssh/id_ed25519" "~/.ssh/id_einherjar" ];
       };
     };
   };
