3f24d163cfd15010d848e2a413e57ce462ed0578
Einherjar <realeinherjar@proton.me>
2023-09-23 09:21:22 -0300

    fix: ssh.knownHosts into system

--- hosts/laptop/wayland/home.nix
+++ hosts/laptop/wayland/home.nix
@@ -11,12 +11,21 @@
     username = "${user}";
     homeDirectory = "/home/${user}";
   };
-  programs = { home-manager.enable = true; };
+  programs = {
+    home-manager.enable = true;
+    ssh = {
+      knownHosts = {
+        "github.com".publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+        "gitlab.com".publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      };
+    };
+  };
 
   home.stateVersion = "23.11";
 
   # Wallpapers
   home.file."wallpaper".source = ./wallpaper;
 }
-
 
