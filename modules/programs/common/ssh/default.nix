826465f2262771ea3eba3c403e12f3dacee4e527
Einherjar <realeinherjar@proton.me>
2023-09-23 09:12:48 -0300

    feat(ssh): better ssh fingerprint opsec

--- modules/programs/common/ssh/default.nix
+++ modules/programs/common/ssh/default.nix
@@ -20,8 +20,14 @@
         hostname = "*";
         forwardAgent = true;
         forwardX11 = true;
-        identityFile = [ "~/.ssh/id_ed25519" "~/.ssh/id_einherjar" ];
+        identityFile = [ "~/.ssh/id_ed25519" ];
       };
+    };
+    knownHosts = {
+      "github.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      "gitlab.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
     };
   };
 }
