d8829475d018f0cadb94a8d379e34d86fc8352ac
Einherjar <realeinherjar@proton.me>
2023-09-23 09:37:10 -0300

    fix: ~/.ssh/known_hosts

--- hosts/laptop/wayland/home.nix
+++ hosts/laptop/wayland/home.nix
@@ -11,17 +11,7 @@
     username = "${user}";
     homeDirectory = "/home/${user}";
   };
-  programs = {
-    home-manager.enable = true;
-    ssh = {
-      knownHosts = {
-        "github.com".publicKey =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-        "gitlab.com".publicKey =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
-      };
-    };
-  };
+  programs = { home-manager.enable = true; };
 
   home.stateVersion = "23.11";
 
--- modules/programs/common/ssh/default.nix
+++ modules/programs/common/ssh/default.nix
@@ -24,5 +24,9 @@
       };
     };
   };
+  home.file.".ssh/know_hosts".text = ''
+    github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+    gitlab.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf
+  '';
 }
 
