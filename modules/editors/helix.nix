698f2b954f9ebb3916b37d39382d6716ff18c040
Einherjar <realeinherjar@proton.me>
2023-09-27 06:04:23 -0300

    feat(helix): languages config (#59)
    * feat(helix): languages config
    
    LSPs, formatters etc
    
    * feat(helix): default editor

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -4,6 +4,7 @@
   programs = {
     helix = {
       enable = true;
+      defaultEditor = true;
       settings = {
         theme = "catppuccin_mocha";
 
@@ -23,9 +24,9 @@
             hidden = false;
           };
           statusline = {
-            left = ["mode" "spinner" "version-control"];
-            center = ["file-name"];
-            right = ["diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type"];
+            left = [ "mode" "spinner" "version-control" ];
+            center = [ "file-name" ];
+            right = [ "diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type" ];
             separator = "│";
             mode.normal = "NORMAL";
             mode.insert = "INSERT";
@@ -38,9 +39,12 @@
         };
         keys = {
           normal = {
-            esc = ["collapse_selection" "keep_primary_selection"];
-            X = ["extend_line_up" "extend_to_line_bounds"];
+            esc = [ "collapse_selection" "keep_primary_selection" ];
+            X = [ "extend_line_up" "extend_to_line_bounds" ];
             "A-x" = "extend_to_line_bounds";
+            "A-j" = [ "ensure_selections_forward" "extend_to_line_bounds" "extend_char_right" "extend_char_left" "delete_selection" "add_newline_below" "move_line_down" "replace_with_yanked" ];
+            "A-k" = [ "ensure_selections_forward" "extend_to_line_bounds" "extend_char_right" "extend_char_left" "delete_selection" "move_line_up" "add_newline_above" "move_line_up" "replace_with_yanked" ];
+
             space = {
               w = ":write";
               q = ":quit";
@@ -51,6 +55,51 @@
             "A-x" = "extend_to_line_bounds";
           };
         };
+      };
+      languages = {
+        language = [
+          {
+            name = "rust";
+            language-server.rust-analyzer.config.check = {
+              command = "clippy";
+            };
+            language-server.rust-analyzer.config.cargo = {
+              features = "all";
+            };
+          }
+          {
+            name = "python";
+            language-server = { command = "ruff-lsp"; };
+            formatter = {
+              command = "black";
+              args = [ "--quiet" "-" ];
+            };
+            auto-format = true;
+          }
+          {
+            name = "toml";
+            formatter = {
+              command = "taplo";
+              args = [ "fmt" "-" ];
+            };
+            auto-format = true;
+          }
+          {
+            name = "yaml";
+            language-server.yaml-language-server.config.yaml = {
+              format = { enable = true; };
+              validation = true;
+            };
+            language-server.yaml-language-server.config.yaml.schemas = {
+              "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml,yaml}";
+              "https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-tasks.json" = "roles/{tasks,handlers}/*.{yml,yaml}";
+            };
+          }
+          {
+            name = "nix";
+          formatter = { command = "nixpkgs-fmt"; };
+          }
+        ];
       };
     };
   };
@@ -63,14 +112,20 @@
       nodePackages_latest.bash-language-server
       nodePackages_latest.vscode-langservers-extracted
       lua-language-server
-      python311Packages.python-lsp-server
       rust-analyzer
       nodePackages_latest.typescript-language-server
       marksman
+      taplo
+      ruff
+      ruff-lsp
+      yaml-language-server
       # tree-sitter
       tree-sitter
       # debugger
       lldb # provides lldb-vscode
+      # formatter
+      nixpkgs-fmt
+      black
     ];
   };
 }
