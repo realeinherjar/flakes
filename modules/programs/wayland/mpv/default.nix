86c55bc1173abc0b5159357265624eebaf478fdc
Einherjar <realeinherjar@proton.me>
2023-11-22 06:31:19 -0300

    feat(mpv): updates from 0.37.0

--- modules/programs/wayland/mpv/default.nix
+++ modules/programs/wayland/mpv/default.nix
@@ -5,36 +5,36 @@
   home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
   home.file.".config/mpv/scripts/file_browser.lua".source = builtins.fetchurl {
     url =
-      "https://github.com/CogentRedTester/mpv-file-browser/raw/87b05d53493b98ff604eba81747f0e32f98dab43/file-browser.lua";
-    sha256 = "a017d9a553d186de03b8bd273b2b4ac650f1150a5a2efb652fcbe3fb32c44c83";
+      "https://github.com/CogentRedTester/mpv-file-browser/raw/3fedbd506dd9ca62c121105ad4d48acdf34bcd53/file-browser.lua";
+    sha256 = "0hqlfcdmmsbry41axhyvjjfd4rcavw75f9awirv2ijh8ivvzh5gj";
   };
   home.file.".config/mpv/scripts-opts/file_browser.conf".source =
     builtins.fetchurl {
       url =
-        "https://github.com/CogentRedTester/mpv-file-browser/raw/f1dd7f359da3d48d7b0234ec2a263efc2e2f8403/file_browser.conf";
+        "https://github.com/CogentRedTester/mpv-file-browser/raw/3fedbd506dd9ca62c121105ad4d48acdf34bcd53/file_browser.conf";
       sha256 =
-        "e216a749217ab9053bff4f36bc15e9ca9d0361c24a2fb1ff85e33e04f5dafc86";
+        "1f82zlar3lbwxpjwygml96py91mdyckagc267cpm97nz2a5hrqgl";
     };
   home.file.".config/mpv/scripts/mpv_thumbnail_script_client_osc.lua".source =
     builtins.fetchurl {
       url =
-        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.2/mpv_thumbnail_script_client_osc.lua";
+        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.3/mpv_thumbnail_script_client_osc.lua";
       sha256 =
-        "36bca9b679697236914b4969f470be52e306270e45f47cce6bfaff9943e44b68";
+        "0xhh8xbm5lhb8wx096xs3rijdrkcq0ndm3s64g6m7x5vk182h64c";
     };
   home.file.".config/mpv/scripts/mpv_thumbnail_script_server-1.lua".source =
     builtins.fetchurl {
       url =
-        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.2/mpv_thumbnail_script_server.lua";
+        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.3/mpv_thumbnail_script_server.lua";
       sha256 =
-        "d8f37051ff18e3a1957abb463c8462ec7e302edcdf07154396a878af0f2d10e7";
+        "18cnajq6ldz0m0fhgdrhknlljmvy8bbd4dg56vp7fqbk4ax34fcq";
     };
   home.file.".config/mpv/scripts/mpv_thumbnail_script_server-2.lua".source =
     builtins.fetchurl {
       url =
-        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.2/mpv_thumbnail_script_server.lua";
+        "https://github.com/marzzzello/mpv_thumbnail_script/releases/download/0.5.3/mpv_thumbnail_script_server.lua";
       sha256 =
-        "d8f37051ff18e3a1957abb463c8462ec7e302edcdf07154396a878af0f2d10e7";
+        "18cnajq6ldz0m0fhgdrhknlljmvy8bbd4dg56vp7fqbk4ax34fcq";
     };
   home.file.".config/mpv/scripts-opts/mpv_thumbnail_script.conf".source =
     ./script-opts/mpv_thumbnail_script.conf;
